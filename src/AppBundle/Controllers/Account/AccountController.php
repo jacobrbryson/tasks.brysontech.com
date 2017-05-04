07bdfb722fd1d8c9704991523d7c377ee6a181a2
Ross Bryson <jacobrbryson@users.noreply.github.com>
2017-05-04 16:15:52 -0400

    Add files via upload

--- src/AppBundle/Controllers/Account/AccountController.php
+++ src/AppBundle/Controllers/Account/AccountController.php
@@ -3,14 +3,23 @@
 namespace AppBundle\Controllers\Account;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Security\Csrf\CsrfTokenManager;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
+use AppBundle\Entity\User;
+use AppBundle\Entity\User_Details;
+
 class AccountController extends Controller{    
+    protected $em;
+    
+    public function __construct(){
+        $this->em = $this->getDoctrine()->getManager();        
+    }
+    
     /**
      * @Route("/login", name="login")
      */
@@ -43,7 +52,7 @@ class AccountController extends Controller{
         $url = 'Web/Account/Register/index.html.twig';
         
         if($request->isMethod('POST') && validateRegistrationInformation($email, $password1, $password2)){
-            //register the USER HERE!!!
+            $this->registerNewUser();
             $url = 'Web/Account/Register/success.html.twig';
         } else if($request->isMethod('POST') && !validateRegistrationInformation($email, $password1, $password2)) {
             $error = "Email already exists within our system.<br/><a href='/resend'>Click Here to reset your password</a>";
@@ -64,6 +73,44 @@ class AccountController extends Controller{
         
     }
     
+    /**
+     * @Route("/resend", name="resend")
+     */
+    public function resendAction(Request $request)
+    {
+        
+    }
+    
+    private function registerNewUser($email){
+        $user           = new User();
+        $userDetails    = new User_Details();
+        $password       = $this->get('security.password_encoder')->encodePassword($user, $pw);
+        $tokenGenerator = $this->container->get('fos_user.util.token_generator');
+        $expirationTime = new \DateTime();
+                $expirationTime->setTimestamp(time() + 3600);
+                
+                $user->setPassword($password)
+                    ->setUserName($email)
+                    ->setEmail($email)
+                    ->setConfirmationToken($tokenGenerator->generateToken());
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                $userDetails->setId($user->getId());
+                $userDetails->setFirstName('New-' . $user->getId());
+                $userDetails->setMiddleName('Awesome');
+                $userDetails->setLastName('User');
+                $userDetails->setBirthday(0);
+                $em->persist($userDetails);
+                $em->flush();
+                
+                $additional['token'] = $user->getConfirmationToken();
+
+                $this->sendConfirmEmail($email, $additional);
+    }
+    
     private function validateRegistrationInformation($email, $password1, $password2){
         return $this->sanitizeEmail($email) && 
             !$this->checkUserExists($email) && 
@@ -75,13 +122,36 @@ class AccountController extends Controller{
     }
     
     private function checkUserExists($email){
-        $em = $this->getDoctrine()->getManager();
-        $exists = $em->getRepository('AppBundle:User')
+        $exists = $this->em->getRepository('AppBundle:User')
             ->findOneBy(array('email' => $email));
         return !$exists ? false : true;
     }
     
     private function verifyPasswords($password1, $password2){
         return $password1 === $password2;
+    }
+    
+    private function sendConfirmEmail($email, $additional){
+        $message = \Swift_Message::newInstance()
+        ->setSubject('Confirmation Email')
+        ->setFrom('task.admin@brysontech.com')
+        ->setTo($email)
+        ->setBody(
+            $this->renderView(
+                'Emails/registration.html.twig',
+                array('email' => $email,
+                    'additional' => $additional,)
+            ),
+            'text/html'
+        )->addPart(
+            $this->renderView(
+                'Emails/registration.txt.twig',
+                array('email' => $email,
+                'additional' => $additional,)
+            ),
+            'text/plain'
+        );
+        
+        $this->get('mailer')->send($message);
     }
 }
