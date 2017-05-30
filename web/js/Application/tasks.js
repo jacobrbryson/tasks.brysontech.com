b0b4bc8eea75a80dfeb9b7d08c8035dc26acb731
jacobrbryson <ross@heels.com>
2017-05-30 14:17:45 -0400

    still grinding on the application and drop-downs and such.

--- web/js/Application/tasks.js
+++ web/js/Application/tasks.js
@@ -1,4 +1,11 @@
-var addTaskForm = document.getElementById("add_tasks");
+var addTaskForm = document.getElementById("form_add_task");
+var addTaskBtn = document.getElementById("btn_add_task");
+
+addTaskBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    
+    $("#modal_add_task").modal();
+});
     
 addTaskForm.addEventListener('submit', function(e){
     e.preventDefault();
@@ -6,7 +13,7 @@ addTaskForm.addEventListener('submit', function(e){
     points = this.points.value;
 
     if(validateNewTask()){
-        addTask(description, points);
+        addTaskBasic(description, points);
     } else {
         alert("Invalid task");
     }
@@ -23,7 +30,7 @@ function validateNewTask(description, points){
     return true;
 }
 
-function addTask(description, points){
+function addTaskBasic(description, points){
     $.ajax({
         type: "POST",
         dataType: "json",
@@ -31,12 +38,8 @@ function addTask(description, points){
         data: {description: description, points: points},
         success: function(response) {
             addTaskForm.reset();
-            $("#tableTasks tr:last").after("<tr id='tasks-tr-"+ response.task_id + "'><td>" + description + "</td>\n\
-                <td>" + points + "</td><td class='td-actions text-right'>\n\
-                <button type='button' onclick='completeTask(" + response.task_id + ");' rel='tooltip' title='Complete Task' class='btn btn-success btn-simple btn-xs'>\n\
-                <i class='fa fa-check' aria-hidden='true'></i></button>\n\
-                <button type='button' onclick='deleteTask(" + response.task_id + ");' rel='tooltip' title='Remove' class='btn btn-danger btn-simple btn-xs'>\n\
-                <i class='fa fa-times'></i></button></td></tr>");
+            $("#tableTasks tr:last").after("<tr id='tasks-tr-" + response.task_id + "'><td>" + description + "</td><td class='text-center'>" + points + "</td><td class='td-actions text-right'><div class='hidden-lg-up text-center'><button type='button'  data-toggle='collapse' data-target='.task-" + response.task_id + "' class='btn btn-secondary btn-simple btn-sm btn-tasks'><i class='fa fa-plus-circle' aria-hidden='true'></i></button></div><div class='hidden-md-down'><button type='button' onclick='editTask(" + response.task_id + ");'rel='tooltip' title='Edit' class='btn btn-info btn-simple btn-sm btn-tasks'><i class='fa fa-pencil' aria-hidden='true'></i></button>&nbsp;<button type='button' onclick='completeTask(" + response.task_id + ");'rel='tooltip' title='Complete' class='btn btn-success btn-simple btn-sm btn-tasks'><i class='fa fa-check' aria-hidden='true'></i></button>&nbsp;<button type='button' onclick='deleteTask(" + response.task_id + ");'rel='tooltip' title='Remove' class='btn btn-danger btn-simple btn-sm btn-tasks'><i class='fa fa-times'></i></button></div></td></tr><tr class='hidden-lg-up'><td colspan='3' style='border-top:none'><div class='navbar-collapse collapse task-" + response.task_id + "'><ul class='navbar-nav ml-auto'><li class='nav-item'><button type='button' onclick='editTask(" + response.task_id + ");'rel='tooltip' title='Edit' class='btn btn-info btn-simple btn-block'><i class='fa fa-pencil' aria-hidden='true'></i>Edit</button></li><li class='nav-item' style='padding-top:10px'><button type='button' onclick='completeTask(" + response.task_id + ");'rel='tooltip' title='Complete' class='btn btn-success btn-simple btn-block'><i class='fa fa-check' aria-hidden='true'></i>Complete</button></li><li class='nav-item' style='padding-top:10px'><button type='button' onclick='deleteTask(" + response.task_id + ");'rel='tooltip' title='Remove' class='btn btn-danger btn-simple btn-block'><i class='fa fa-times'></i>Delete</button></li></ul></div></td></tr>");
+            $("#modal_add_task").modal('toggle');
         },
         error: function(){
             title   = "Something went wrong.";
@@ -45,6 +48,18 @@ function addTask(description, points){
             showAlert(title, body);
         }
     });
+}
+
+function editTask(taskid){
+    //get task information
+    
+    //show task information in the modal
+}
+
+function submitTaskEdits(data){
+    //serialize data
+    
+    //pass it over via ajax to the controller, let her do her job.
 }
 
 function completeTask(taskid){
@@ -96,3 +111,4 @@ function deleteTask(taskid){
         }
     });
 }
+
