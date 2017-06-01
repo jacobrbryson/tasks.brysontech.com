c884e708e43a08d17bb6b45ecc7ef7aa8cf30ebb
Ross Bryson <ross@brysontech.com>
2017-05-31 21:16:26 -0400

    Fixed the clock and made the navbar sticky.

--- web/js/Application/tasks.js
+++ web/js/Application/tasks.js
@@ -1,5 +1,6 @@
 var addTaskForm = document.getElementById("form_add_task");
 var addTaskBtn = document.getElementById("btn_add_task");
+var navDate = document.getElementById("nav_date");
 
 addTaskBtn.addEventListener('click', function(e){
     e.preventDefault();
@@ -112,3 +113,48 @@ function deleteTask(taskid){
     });
 }
 
+function updateClock(){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var M = today.getMonth();
+    var day = today.getDay();
+    var year = today.getFullYear();
+    var amPM = "a";
+    amPM = checkAMPM(h);
+    h = fixHour(h);
+    m = checkTime(m);
+    
+    
+    
+    navDate.innerHTML = M + "/" + day + "/" + year + " " + h + ":" + m + amPM;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    };  // add zero in front of numbers < 10
+        return i;
+    }
+    
+    function fixHour(h){
+        if(h > 12){
+            h = h - 12;
+        }
+        return h;
+    }
+    
+    function checkAMPM(h){
+        if(h > 11){
+            return "p";
+        } else {
+            return "a";
+        }
+        
+    }
+
+updateClock();
+
+clock = setInterval(function() {
+    updateClock();
+}, 60 * 1000);
