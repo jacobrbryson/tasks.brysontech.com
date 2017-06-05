21e7cf2a1267ce8fd77ba72095d9e60102f72a29
JJones <jjones@brysontech.com>
2017-06-05 15:24:08 -0400

    Adding a popup

--- web/js/Application/tasks.js
+++ web/js/Application/tasks.js
@@ -113,3 +113,9 @@ function deleteTask(taskid){
         }
     });
 }
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
