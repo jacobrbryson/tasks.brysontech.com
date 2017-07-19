db12dbf002e5dc37f43d18af5ea0248ec5967121
JJones <jjones@brysontech.com>
2017-07-19 11:43:19 -0400

    Working with branches

--- src/AppBundle/Controllers/Account/AccountController.php
+++ src/AppBundle/Controllers/Account/AccountController.php
@@ -13,6 +13,8 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use AppBundle\Entity\User;
 use AppBundle\Entity\User_Details;
 
+use AppBundle\Libraries\Facebook;
+
 class AccountController extends Controller{    
     /**
      * @Route("/register", name="register")
@@ -70,9 +72,23 @@ class AccountController extends Controller{
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         
+        $fb = new Facebook\Facebook([
+            'app_id' => '471244956601440',
+            'app_secret' => '7ebf1f926ef984b04f3956e926357e57',
+            'default_graph_version' => 'v2.2',
+            ]);
+
+          $helper = $fb->getRedirectLoginHelper();
+
+          $permissions = ['email']; // Optional permissions
+          $loginUrl = $helper->getLoginUrl('http://local.tasks.brysontech.com/fb-login', $permissions);
+
+          $fbButton = '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+        
         return $this->render('Web/Account/Login/index.html.twig',[
             'error'         => $error,
-            'lastUsername'  => $lastUsername
+            'lastUsername'  => $lastUsername,
+            'fbButton'      => $fbButton
         ]);
     }
     
@@ -82,6 +98,83 @@ class AccountController extends Controller{
     public function resendAction(Request $request)
     {
         
+    }
+    
+     /**
+     * @Route("login/check-facebook", name="check-facebook")
+     */
+    public function facebookLoginAction(Request $request)
+    {
+        $fb = new Facebook\Facebook([
+            'app_id' => '471244956601440', // Replace {app-id} with your app id
+            'app_secret' => '7ebf1f926ef984b04f3956e926357e57',
+            'default_graph_version' => 'v2.2',
+            ]);
+
+          $helper = $fb->getRedirectLoginHelper();
+
+          try {
+            $accessToken = $helper->getAccessToken();
+          } catch(Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+          } catch(Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+          }
+
+          if (! isset($accessToken)) {
+            if ($helper->getError()) {
+              header('HTTP/1.0 401 Unauthorized');
+              echo "Error: " . $helper->getError() . "\n";
+              echo "Error Code: " . $helper->getErrorCode() . "\n";
+              echo "Error Reason: " . $helper->getErrorReason() . "\n";
+              echo "Error Description: " . $helper->getErrorDescription() . "\n";
+            } else {
+              header('HTTP/1.0 400 Bad Request');
+              echo 'Bad request';
+            }
+            exit;
+          }
+
+          // Logged in
+          echo '<h3>Access Token</h3>';
+          var_dump($accessToken->getValue());
+
+          // The OAuth 2.0 client handler helps us manage access tokens
+          $oAuth2Client = $fb->getOAuth2Client();
+
+          // Get the access token metadata from /debug_token
+          $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+          echo '<h3>Metadata</h3>';
+          var_dump($tokenMetadata);
+
+          // Validation (these will throw FacebookSDKException's when they fail)
+          $tokenMetadata->validateAppId('471244956601440'); // Replace {app-id} with your app id
+          // If you know the user ID this access token belongs to, you can validate it here
+          //$tokenMetadata->validateUserId('123');
+          $tokenMetadata->validateExpiration();
+
+          if (! $accessToken->isLongLived()) {
+            // Exchanges a short-lived access token for a long-lived one
+            try {
+              $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+            } catch (Facebook\Exceptions\FacebookSDKException $e) {
+              echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+              exit;
+            }
+
+            echo '<h3>Long-lived</h3>';
+            var_dump($accessToken->getValue());
+          }
+
+          $_SESSION['fb_access_token'] = (string) $accessToken;
+
+          // User is logged in with a long-lived access token.
+          // You can redirect them to a members-only page.
+          return $this->redirectToRoute('application');
     }
     
     private function enableUser($email, $token){
