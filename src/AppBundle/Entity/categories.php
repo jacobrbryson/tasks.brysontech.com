41dc8c063253defe41bce9d3b465e48acd899417
JJones <jjones@brysontech.com>
2017-07-26 15:39:53 -0400

    Generated getters and setters

--- src/AppBundle/Entity/categories.php
+++ src/AppBundle/Entity/categories.php
@@ -20,3 +20,39 @@ class Category{
      * @ORM\Column(type="text")
      */
     private $name;
+    
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tasks
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+    
+     /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+}
