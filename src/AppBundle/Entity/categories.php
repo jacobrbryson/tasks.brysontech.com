5894b1f422013b4637786124ca7274c80487a2f0
JJones <jjones@brysontech.com>
2017-07-26 14:41:20 -0400

    Added the categories.php entity

---
+++ src/AppBundle/Entity/categories.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+class Category{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
