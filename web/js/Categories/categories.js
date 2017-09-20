ab368a147c279660460e687b471433adbe4c239d
JJones <jjones@brysontech.com>
2017-09-20 12:45:52 -0400

    Making Javascript for addCategories.

---
+++ web/js/Categories/categories.js
@@ -0,0 +1,15 @@
+function addCategory(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "/tasks/addCategory",
+        data: {name: "Name of Category"},
+        success: function(response) {
+            alert("This Works");
+        },
+        error: function(){
+            popup("Ajax Error - Refresh and try again.");
+        }
+    });
+
+
