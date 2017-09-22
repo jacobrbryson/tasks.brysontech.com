1efaeaefd9e1b3a8013cbcaee1f0862f81676e81
JJones <jjones@brysontech.com>
2017-09-22 12:09:14 -0400

    Working on js for adding categories.

--- web/js/Categories/categories.js
+++ web/js/Categories/categories.js
@@ -1,11 +1,28 @@
+var addCategoryForm = document.getElementById("add_category_form");
+var addCatgoryBtn = document.getElementById("add_category_btn");
+
+addCategoryBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    //$("#modal_add_task").modal();
+    //$("#start_date_time").val(Math.round(new Date().getTime()/1000.0));
+    //$("#end_date_time").val(Math.round(new Date().getTime()/1000.0) + 86400);
+});
+
+addCategoryForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    addCategory();
+});
+
 function addCategory(){
+    $data = $("#form_add_task").serialize();
+    
     $.ajax({
         type: "POST",
         dataType: "json",
         url: "/tasks/addCategory",
-        data: {name: "Name of Category"},
+        data: {data: $data},
         success: function(response) {
-            alert("This Works");
+           popup("New Category Added");;
         }
         /*error: function(){
             popup("Ajax Error - Refresh and try again.");
