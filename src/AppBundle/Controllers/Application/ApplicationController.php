ff0d81fcb11ba1b712c84f57325a583e4a9427b4
ross.bryson <ross.bryson@SLC-LT-RB-1.mshome.net>
2018-02-16 15:21:24 -0600

    done again?

--- src/AppBundle/Controllers/Application/ApplicationController.php
+++ src/AppBundle/Controllers/Application/ApplicationController.php
@@ -27,7 +27,7 @@ class ApplicationController extends Controller{
         $em = $this->getDoctrine()->getManager();
 
         $connection = $em->getConnection();
-        $statement = $connection->prepare("SELECT DISTINCT(t.categoryId) as 'categoryId', c.name as 'name' FROM tasks t JOIN categories c ON t.categoryId = c.Id WHERE owner = :owner AND complete = 0");
+        $statement = $connection->prepare("SELECT DISTINCT(t.categoryId) as 'categoryId', c.name as 'name' FROM tasks t JOIN categories c ON t.categoryId = c.Id WHERE owner = :owner AND complete = 0 ORDER BY categoryId asc");
         $statement->bindValue('owner', $this->getUser()->getId());
         $statement->execute();
         $distictCategories = $statement->fetchAll();
