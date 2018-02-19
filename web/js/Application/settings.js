187246b8016dfd537e29eff06ac4a20b7d9636f2
rbryson <rbryson@10.0.0.35>
2018-02-19 14:25:50 -0600

    Added the ability to add category to the settings page.

--- web/js/Application/settings.js
+++ web/js/Application/settings.js
@@ -18,3 +18,40 @@ function populateCategories(categories){
         $('#select_category').append($('<option>', {value:categories[i].id, text:categories[i].name}));
     }
 }
+
+function addCategory(name){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "/tasks/addCategory",
+        data:{name:name},
+        success: function(response) {
+            populateCategories(response.data);
+            popup(response.data[0].name + " added.");
+            $("#form_categories")[0].reset();
+        },
+        error: function(){
+            popup("Ajax Error - Refresh and try again.");
+        }
+    });
+}
+
+function deleteCategory(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "/tasks/addCategories",
+        success: function(response) {
+            populateCategories(response.data);
+        },
+        error: function(){
+            popup("Ajax Error - Refresh and try again.");
+        }
+    });
+}
+
+$("#form_categories").on("submit", function(e){
+    e.preventDefault();
+    
+    addCategory($('#category_name').val());
+});
